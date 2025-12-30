95f2ad60faabf04a44e5c61a27d9a856039dd224
papawin <frame2346@gmail.com>
2025-12-30 17:37:44 +0700

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
+  // เอา LayoutComponent คลุมไว้แล้ว เติม path ข้างล่างต่อได้เลย เช็ค import ให้ดี
   {
     path: '',
     component: LayoutComponent,
